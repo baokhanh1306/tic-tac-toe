a2904d2e70b2f692f030b280a88140f68c3814dd
Khanh Bảo <baokhanh1306nsc@gmail.com>
2020-10-12 20:43:04 +0700

    finish tutorial

--- src/App.js
+++ src/App.js
@@ -1,55 +1,77 @@
 import React, { useState } from "react";
-import styles from './App.module.css';
-import './components/Board/Board.module.css';
-import './components/Square/Square.module.css';
+import styles from "./App.module.css";
+import "./components/Board/Board.module.css";
+import "./components/Square/Square.module.css";
 import Board from "./components/Board/Board";
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setXisNext] = useState(true);
-  const winner = calculateWinner(board);
+  const [stepNumber, setStepNumber] = useState(0);
+
+  const winner = calculateWinner(history[stepNumber]);
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : `Go to game start`;
+    return (
+      <li key={move}>
+        <button className={styles.btn} onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
   const handleClick = (i) => {
-    const boardCopy = [...board];
-    if (winner || boardCopy[i]) return;
-    boardCopy[i] = xIsNext ? "X" : "0";
-    setBoard(boardCopy);
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
+
+    if (winner || squares[i]) return;
+
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
     setXisNext(!xIsNext);
   };
-  const handleRetry = () => {
-    setBoard(Array(9).fill(null));
-    setXisNext(true);
-  }
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
+
   return (
     <div className={styles.main}>
-      <h3>{winner ? "Winner: " + winner : "Next Player: " + (xIsNext ? "X" : "O")}</h3>
-      <Board squares={board} onClick={handleClick} />
-      <button className={styles.btn} onClick={handleRetry}>
-        Retry
-      </button>
+      <div className={styles.container}>
+        <h3>
+          {winner
+            ? "Winner: " + winner
+            : "Next Player: " + (xIsNext ? "X" : "O")}
+        </h3>
+        <Board squares={history[stepNumber]} onClick={handleClick} />
+      </div>
+      <ol>{moves}</ol>
     </div>
   );
 };
 
-
 function calculateWinner(squares) {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-	for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
-		}
-	}
-	return null;
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
-
 
 export default App;
